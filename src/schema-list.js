81921fa771ce2913af30a6876b2bc313a8bfe0a7
Marijn Haverbeke <marijn@haverbeke.nl>
2019-10-08 11:42:27 +0200

    Use default node type from start of item when splitting
    See https://discuss.prosemirror.net/t/possible-regression-in-splitlistitem-prosemirror-schema-list/2257
    
    FIX: Fix regression where `splitListItem` doesn't work at the end of an item when
    the content for list items has different first and non-first allowed nodes.

--- src/schema-list.js
+++ src/schema-list.js
@@ -140,7 +140,7 @@ export function splitListItem(itemType) {
       }
       return true
     }
-    let nextType = $to.pos == $from.end() ? grandParent.contentMatchAt($from.indexAfter(-1)).defaultType : null
+    let nextType = $to.pos == $from.end() ? grandParent.contentMatchAt(0).defaultType : null
     let tr = state.tr.delete($from.pos, $to.pos)
     let types = nextType && [null, {type: nextType}]
     if (!canSplit(tr.doc, $from.pos, 2, types)) return false
